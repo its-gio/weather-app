393d26d880f800e831a9b04125e7fc6970a5610e
its-gio <me@its-g.io>
2018-07-15 22:42:07 -0700

    Starting form and basic API call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Title from './components/Title';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "145e1b689df6ae9387c0fea9f65bb355";
+
 export default class App extends React.Component{
+  // Making api call is best with async and await
+  getWeather = async () => {
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render(){
     return(
       <React.Fragment>
