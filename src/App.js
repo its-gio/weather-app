85445305ec160303bbfde8fd455f352be7a0464d
its-gio <me@its-g.io>
2018-07-15 22:55:13 -0700

    console logging api call for fremont

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ const API_KEY = "145e1b689df6ae9387c0fea9f65bb355";
 
 export default class App extends React.Component{
   // Making api call is best with async and await
-  getWeather = async () => {
-    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`);
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=94555,us&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
   }
@@ -17,7 +18,7 @@ export default class App extends React.Component{
     return(
       <React.Fragment>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </React.Fragment>
     )
