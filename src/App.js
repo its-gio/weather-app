58aa835ccef2b4486b71db41d518695256eff575
its-gio <me@its-g.io>
2018-07-15 23:20:16 -0700

    Setting default states

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Weather from './components/Weather';
 const API_KEY = "145e1b689df6ae9387c0fea9f65bb355";
 
 export default class App extends React.Component{
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    desc: undefined,
+    err: undefined
+  }
+
   // Making API call is best with async and await
   getWeather = async (e) => {
     e.preventDefault();
@@ -19,6 +28,7 @@ export default class App extends React.Component{
     const data = await api_call.json();
     console.log(data);
   }
+
   render(){
     return(
       <React.Fragment>
