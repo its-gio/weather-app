e8cf9afaad8099ef1278bfb369f76b37460917fb
its-gio <me@its-g.io>
2018-07-15 23:16:42 -0700

    Adding console log of form submit to api call

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import Weather from './components/Weather';
 const API_KEY = "145e1b689df6ae9387c0fea9f65bb355";
 
 export default class App extends React.Component{
-  // Making api call is best with async and await
+  // Making API call is best with async and await
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=fremont,us&appid=${API_KEY}&units=imperial`);
+    // Grabbing city and country front component form
+    const city = e.target.city.value;
+    const country = e.target.country.value;
+    // Making API call
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    // Turning API call into json
     const data = await api_call.json();
     console.log(data);
   }
