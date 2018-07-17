9b69ec100e5e5555d0183ce8236f352416013e40
its-gio <me@its-g.io>
2018-07-16 17:14:55 -0700

    Changing Title.js to stateless component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export default class Title extends React.Component{
-  render(){
-    return(
-      <div>
-          <h1>Weather Finder</h1>
-          <p>Find the temperature, conditions and more...</p>
-      </div>
-    )
-  }
-}
+const Title = () => (
+  <div>
+      <h1>Weather Finder</h1>
+      <p>Find the temperature, conditions and more...</p>
+  </div>
+)
+
+export default Title;
