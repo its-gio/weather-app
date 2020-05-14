6a09903638cbb61b197d4df1afd694c924c53790
Giovani Aguirre <me@its-g.io>
2020-05-14 15:55:34 -0700

    Toggle added to modal form.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Working!")
+const modal = document.querySelector("#modal");
+const exitModalBtn = document.querySelector("#exit-modal");
+const enterModalBtn = document.querySelector("#change-location-btn");
+let modalHidden = true;
+
+const toggleModal = () => modal.classList.toggle("hidden");
+
+exitModalBtn.addEventListener('click', toggleModal);
+enterModalBtn.addEventListener('click', toggleModal);
