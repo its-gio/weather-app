74a9f1b50fadedd4f7800a62ef45e1a26160b456
Giovani Aguirre <me@its-g.io>
2020-05-18 16:34:21 -0700

    Required all files into index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,2 @@
-const modal = document.querySelector("#modal");
-const exitModalBtn = document.querySelector("#exit-modal");
-const enterModalBtn = document.querySelector("#change-location-btn");
-let modalHidden = true;
-
-const toggleModal = () => modal.classList.toggle("hidden");
-
-exitModalBtn.addEventListener('click', toggleModal);
-enterModalBtn.addEventListener('click', toggleModal);
+require('./Weather');
+require('./modalToggle');
