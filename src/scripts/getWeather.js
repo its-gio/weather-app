a1e82076b40d7d395ac8aa702056c6c692972968
Giovani Aguirre <me@its-g.io>
2020-05-20 13:29:20 -0700

    Successfully getting geo location of city and state and plugging into dark sky api for weather response,

--- src/scripts/getWeather.js
+++ src/scripts/getWeather.js
@@ -2,13 +2,24 @@ export default class Weather {
   constructor(city, state) {
     this.proxy = "https://cors-anywhere.herokuapp.com/";
     this.dsAPI = process.env.DARK_SKY_KEY;
+    this.geoAPI = process.env.GOECODE_KEY;
     this.city = city;
     this.state = state;
+    this.locationCombined = '';
+    this.long = null;
+    this.latt = null;
   }
 
   async getWeather() {
+    this.locationCombined = `${this.city.split(" ").join("+")}+${this.state}`;
+
     try {
-      const response = await fetch(`${this.proxy}https://api.darksky.net/forecast/${this.dsAPI}/34.00469,-118.30136`)
+      let getLongLatt = await fetch(`https://geocode.xyz/${this.locationCombined}?json=1`)
+          .then(blob => blob.json())
+      this.latt = getLongLatt.latt;
+      this.long = getLongLatt.longt;
+
+      const response = await fetch(`${this.proxy}https://api.darksky.net/forecast/${this.dsAPI}/${this.latt},${this.long}`)
         .then(blob => blob.json());
       
       return response.currently;
